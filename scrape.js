81768ead35a3d159902db0d7c32e8edf614daf8b
Nick Lee <nicklee808@gmail.com>
2018-03-05 00:16:18 -1000

    sliced PART NUMBER from string

--- scrape.js
+++ scrape.js
@@ -1,66 +1,29 @@
-// const puppeteer = require('puppeteer');
-
-// let scrape = async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   // await page.goto('http://books.toscrape.com/');
-
-//   // const result = await page.evaluate(() => {
-//   //   let data = [];
-//   //   let elements = document.querySelectorAll('.product_pod');
-//   //     for (var element of elements){
-//   //       let title = element.childNodes[5].innerText;
-//   //       let price = element.childNodes[7].children[0].innerText;
-//   //       data.push({title, price});
-//   //     }
-//   //     return data;
-//   // }
-
-//   await page.goto('https://www.reliableparts.com/catalog/air_conditioner_filters');
-//   await page.waitFor(1000);
-
-
-//   await page.click('#middle > div.container > div.row.subcategory-row > div:nth-child(1) > div > div.box-bottom > h2 > a');
-
-//   const result = await page.evaluate(() => {
-//     let title = document.querySelector('h1').innerText;
-//     return title;
-//   });
-
-
-//   browser.close();
-//   return result;
-// };
-
-// scrape().then((value) => {
-//     console.log(value);
-// });
-
 const puppeteer = require('puppeteer');
+// All air conditionar parts
 const urls = [
   'https://www.reliableparts.com/catalog/air_conditioner_filters',
   'https://www.reliableparts.com/catalog/660109',
   'https://www.reliableparts.com/catalog/659561',
-  // 'https://www.reliableparts.com/catalog/660110',
-  // 'https://www.reliableparts.com/catalog/660108',
-  // 'https://www.reliableparts.com/catalog/660099',
-  // 'https://www.reliableparts.com/catalog/660100',
-  // 'https://www.reliableparts.com/catalog/660115',
-  // 'https://www.reliableparts.com/catalog/660124',
-  // 'https://www.reliableparts.com/catalog/660125',
-  // 'https://www.reliableparts.com/catalog/660126',
-  // 'https://www.reliableparts.com/catalog/660114',
-  // 'https://www.reliableparts.com/catalog/air_conditioner_repair_tools',
-  // 'https://www.reliableparts.com/catalog/compressor_hard_start_kits',
-  // 'https://www.reliableparts.com/catalog/648800',
-  // 'https://www.reliableparts.com/catalog/electronic_control_boards',
-  // 'https://www.reliableparts.com/catalog/motor_mounting_bracket_kits',
-  // 'https://www.reliableparts.com/catalog/a_c_air_conditioner_motors',
-  // 'https://www.reliableparts.com/catalog/motor_run_capacitors',
-  // 'https://www.reliableparts.com/catalog/motor_start_capacitors',
-  // 'https://www.reliableparts.com/catalog/time_delay_timers',
-  // 'https://www.reliableparts.com/catalog/relays_',
-  // 'https://www.reliableparts.com/catalog/transformers_'
+  'https://www.reliableparts.com/catalog/660110',
+  'https://www.reliableparts.com/catalog/660108',
+  'https://www.reliableparts.com/catalog/660099',
+  'https://www.reliableparts.com/catalog/660100',
+  'https://www.reliableparts.com/catalog/660115',
+  'https://www.reliableparts.com/catalog/660124',
+  'https://www.reliableparts.com/catalog/660125',
+  'https://www.reliableparts.com/catalog/660126',
+  'https://www.reliableparts.com/catalog/660114',
+  'https://www.reliableparts.com/catalog/air_conditioner_repair_tools',
+  'https://www.reliableparts.com/catalog/compressor_hard_start_kits',
+  'https://www.reliableparts.com/catalog/648800',
+  'https://www.reliableparts.com/catalog/electronic_control_boards',
+  'https://www.reliableparts.com/catalog/motor_mounting_bracket_kits',
+  'https://www.reliableparts.com/catalog/a_c_air_conditioner_motors',
+  'https://www.reliableparts.com/catalog/motor_run_capacitors',
+  'https://www.reliableparts.com/catalog/motor_start_capacitors',
+  'https://www.reliableparts.com/catalog/time_delay_timers',
+  'https://www.reliableparts.com/catalog/relays_',
+  'https://www.reliableparts.com/catalog/transformers_'
 ];
 
 
@@ -105,7 +68,7 @@ let scrape = async () => {
       let elements = document.querySelectorAll('.box-bottom');
       for (var i = 0; i < elements.length; i++){
         let title = elements[i].childNodes[1].innerText;
-        let partNum = elements[i].childNodes[3].innerText;
+        let partNum = elements[i].childNodes[3].innerText.slice(13);
         let price = elements[i].childNodes[5].innerText;
         data.push({title, partNum, price});
       }
@@ -117,6 +80,6 @@ let scrape = async () => {
   return result;
 };
 
-scrape().then((value) => {
-  console.log('value: ', value);
+scrape().then((scrapedData) => {
+  console.log('SCRAPED DATA: ', scrapedData);
 });
